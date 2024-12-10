fa75e79d3c2cb766c9fd9aedc98c535747dc0605
keshavsara <keshavsara8@gmail.com>
2024-12-10 19:45:27 +0530

    PaintersPartitionUpdate

--- Arrays/PaintersPartition.cpp
+++ Arrays/PaintersPartition.cpp
@@ -1,62 +1,53 @@
 #include<iostream>
 #include<vector>
+#include<climits>  
 using namespace std;
-bool isvalid(vector<int>&arr,int n,int m,int maxAllowedWalls)
-{
-    int Painters=1,units=0;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]>maxAllowedWalls)
-        {
+
+bool isPossible(vector<int> &arr, int n, int m, int maxAllowedTime) {
+    int painters = 1, time = 0;
+    for(int i = 0; i < n; i++) {
+        
+        if (arr[i] > maxAllowedTime) {
             return false;
         }
-        if(units+arr[i]<=maxAllowedWalls)
-        {
-            units+=arr[i];
+        
+        if (time + arr[i] <= maxAllowedTime) {
+            time += arr[i];
+        } else {
+            painters++;
+            time = arr[i];
         }
-        else{
-            Painters++;
-            units=arr[i];
+        if (painters > m) {
+            return false;
         }
     }
-    return Painters>m ? false:true;
+    return true;
 }
 
-int MinCost(vector<int> &arr, int n,int m)
-{
-    if(m>n) return -1;
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-        sum+=arr[i];
+int minTimeToPaint(vector<int> &arr, int n, int m) {
+    int sum = 0, maxVal = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
     }
-    int st=0,end=sum;
-    int ans=-1;
-    if(m>n)
-    {
-        return -1;
-    }
-    while(st<=end)
-    {
-        int mid=st+(end-st)/2;
-        if(isvalid(arr,n,m,mid))
-        {
-            ans=mid;
-            end=mid-1;
-        }
-        else
-        {
-            st=mid+1;
+
+    int st = maxVal, end = sum, ans = -1;
+    while (st <= end) {
+        int mid = st + (end - st) / 2; 
+        
+        if (isPossible(arr, n, m, mid)) {
+            ans = mid;
+            end = mid - 1;
+        } else {
+            st = mid + 1;
         }
     }
     return ans;
 }
 
-int main()
-{
+int main() {
     vector<int> arr = {40, 30, 10, 20};
-    int n = 4; 
-    int m = 2; 
-    cout<<"Minimum cost :" << MinCost(arr,n,m)<<endl;
+    int n = 4, m = 2;
+    cout << minTimeToPaint(arr, n, m) << endl;
     return 0;
 }
